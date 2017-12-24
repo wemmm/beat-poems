54f774774a7081d2f533aabab0e9a72aa2d8690e
wemmm <jenny.wem@gmail.com>
2017-12-24 11:27:14 +0000

    Refactors Poem to a stateful class component, configures shuffle button

--- src/Poem.js
+++ src/Poem.js
@@ -2,19 +2,39 @@ import React, { Component } from 'react';
 import lines from './lines.json';
 import './Line.css';
 
-const Line = (props) => {
-  return (
-    <div>
-      <p>{props.label}</p>
-      <h2>{lines.line1[1]}</h2>
-      <h2>{lines.line2[0]}</h2>
-      <h2>{lines.line3[2]}</h2>
-      <input
-        type="button"
-        onClick={props.handleClick}
-      />
-    </div>
-  );
+class Poem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      poemLineOne: 0,
+      poemLineTwo: 0,
+      poemLineThree: 0
+    }
+  }
+
+  randomNumber = () => {
+    return Math.floor(Math.random() * (3 - 0)) + 0;
+  }
+
+  shuffleLines = () => {
+    this.setState({
+      poemLineOne: this.randomNumber()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>{this.props.label}</p>
+        <h2>{lines.line1[this.state.poemLineOne]}</h2>
+        <h2>{lines.line2[0]}</h2>
+        <h2>{lines.line3[2]}</h2>
+        <input
+          type="button"
+          onClick={this.shuffleLines}
+        />
+      </div>
+  )};
 }
 
-export default Line;
+export default Poem;
