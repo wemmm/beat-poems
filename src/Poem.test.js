5a5dd7914b69ec087835b1b507d7a0ed4d6acc38
wemmm <jenny.wem@gmail.com>
2018-01-07 21:32:47 +0000

    Tests for button presence

--- src/Poem.test.js
+++ src/Poem.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import Poem from './Poem';
 import { Button, ButtonGroup } from 'react-bootstrap';
 import ShareButton from 'react-social-share-buttons'
@@ -28,4 +28,33 @@ it('renders the second line of a poem', () => {
 it('renders the third line of a poem', () => {
   const wrapper = shallow(<Poem label="test"/>);
   expect(wrapper.find('h2').at(2).length).not.toBe(0);
+});
+
+it('renders shuffle buttons for each line', () => {
+  const wrapper = shallow(<Poem />);
+  const label = "Shuffle"
+  const button1 = (wrapper.find('Button').at(0));
+  const button2 = (wrapper.find('Button').at(2));
+  const button3 = (wrapper.find('Button').at(4));
+  expect(button1.contains(label)).toEqual(true);
+  expect(button2.contains(label)).toEqual(true);
+  expect(button3.contains(label)).toEqual(true);
+});
+
+it('renders lock buttons for each line', () => {
+  const wrapper = shallow(<Poem />);
+  const label = "Lock"
+  const button1 = (wrapper.find('Button').at(1));
+  const button2 = (wrapper.find('Button').at(3));
+  const button3 = (wrapper.find('Button').at(5));
+  expect(button1.contains(label)).toEqual(true);
+  expect(button2.contains(label)).toEqual(true);
+  expect(button3.contains(label)).toEqual(true);
+});
+
+it('renders a shuffle all button', () => {
+  const wrapper = shallow(<Poem />);
+  const label = "Shuffle All"
+  const button = (wrapper.find('Button').at(6));
+  expect(button.contains(label)).toEqual(true);
 });
